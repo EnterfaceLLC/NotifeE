2e9f3bdcedbd02be0d4c738fb2b09f6ce723a203
EnterfaceLLC <dotenterface@gmail.com>
2024-11-10 16:58:02 -0600

    +Successful Push Notification on device!

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,9 +2,12 @@ import { StyleSheet, Text, View, Button, Alert } from "react-native";
 
 import notifee, { EventType } from "@notifee/react-native";
 import { useEffect, useState } from "react";
-import messaging from "@react-native-firebase/messaging";
+import messaging, { FirebaseMessagingTypes } from "@react-native-firebase/messaging";
 
 export default function TabOneScreen() {
+  const [notifTitle, setNotifTitle] = useState("");
+  const [notifBody, setNotifBody] = useState<FirebaseMessagingTypes.RemoteMessage | string>("");
+
   async function requestUserPermission() {
     const authStatus = await messaging().requestPermission();
     const enabled =
@@ -28,85 +31,54 @@ export default function TabOneScreen() {
 
     const unsubscribe = messaging().onMessage(async (remoteMessage) => {
       Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+      console.log(
+        "FCM message Body{}!",
+        JSON.stringify(remoteMessage.notification?.body)
+      );
+      setNotifBody(remoteMessage.notification?.title);
+      setNotifBody(remoteMessage.notification?.body);
     });
 
     return unsubscribe;
   });
 
-  ///////////
-  // useEffect(() => {
-  //   const reqPerm = async () => {
-  //     try {
-  //       await messaging().requestPermission();
+  async function onDisplayNotification() {
+    // Request permissions (required for iOS)
+    await notifee.requestPermission();
 
-  //       await messaging().registerDeviceForRemoteMessages();
-  //       const check = messaging().isDeviceRegisteredForRemoteMessages;
-  //       // const token = await messaging().getToken();
-  //       // console.log("TOKEN:", token);
-  //       console.log("CHECK:", check);
-  //     } catch (error) {
-  //       console.log("PERM-TOKEN ERROR", error);
-  //     }
-  //   };
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: "default",
+      name: "Default Channel",
+    });
 
-  //   reqPerm();
-  // }, []);
-
-  // useEffect(() => {
-  //   requestUserPermission();
-  //   const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-  //     Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
-  //   });
-
-  //   return unsubscribe;
-  // }, []);
-
-  // async function requestUserPermission() {
-  //   const authStatus = await messaging().requestPermission();
-  //   const enabled =
-  //     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-  //     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-  //   if (enabled) {
-  //     console.log("Authorization status:", authStatus);
-  //   }
-  // }
-
-  // async function onDisplayNotification() {
-  //   // Request permissions (required for iOS)
-  //   await notifee.requestPermission();
-
-  //   // Create a channel (required for Android)
-  //   const channelId = await notifee.createChannel({
-  //     id: "default",
-  //     name: "Default Channel",
-  //   });
-
-  //   // Display a notification
-  //   await notifee.displayNotification({
-  //     title: ".Enterface 📱",
-  //     body: "Be sure to download mobile Munch!",
-  //     android: {
-  //       channelId,
-  //       color: "#ff0066",
-  //       smallIcon: "name-of-a-small-icon", // optional, defaults to 'ic_launcher'.
-  //       // pressAction is needed if you want the notification to open the app when pressed
-  //       pressAction: {
-  //         id: "default",
-  //       },
-  //     },
-  //     ios: {
-  //       attachments: [
-  //         {
-  //           url: "https://mobilemunchmedia.s3.us-east-1.amazonaws.com/Notifs/BestBuyTrks.png",
-  //         },
-  //       ],
-  //       foregroundPresentationOptions: {
-  //         badge: true,
-  //       },
-  //     },
-  //   });
-  // }
+    // Display a notification
+    await notifee.displayNotification({
+      // title: ".Enterface 📱",
+      title: notifTitle,
+      // body: "Be sure to download mobile Munch!",
+      body: notifBody,
+      android: {
+        channelId,
+        color: "#ff0066",
+        smallIcon: "name-of-a-small-icon", // optional, defaults to 'ic_launcher'.
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: "default",
+        },
+      },
+      ios: {
+        attachments: [
+          {
+            url: "https://mobilemunchmedia.s3.us-east-1.amazonaws.com/Notifs/BestBuyTrks.png",
+          },
+        ],
+        foregroundPresentationOptions: {
+          badge: true,
+        },
+      },
+    });
+  }
 
   // useEffect(() => {
   //   return notifee.onForegroundEvent(({ type, detail }) => {
